3dd65634606834cc88e09cc888e2718ddf57a677
Ritik katiyar <55633948+ritikkatiyar@users.noreply.github.com>
2025-04-22 20:18:54 +0530

    vending machine

---
+++ Vending machine/HasMoneyState.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class HasMoneyState implements State{
+    @Override
+    public void clickOnInsertCoinButton(VendingMachine vendingMachine) throws Exception {
+
+    }
+
+    @Override
+    public void clickOnStartProductSelectionButton(VendingMachine vendingMachine) throws Exception {
+
+    }
+
+    @Override
+    public void insertCoin(VendingMachine vendingMachine, Coin coin) throws Exception {
+
+    }
+
+    @Override
+    public void chooseProduct(VendingMachine machine, int codeNumber) throws Exception {
+
+    }
+
+    @Override
+    public int getChange(int returnChangeMoney) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public Item dispenseProduct(VendingMachine vendingMachine, int codeNumber) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Coin> refundFullMoney(VendingMachine vendingMachine) throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public void updateInventory(VendingMachine vendingMachine, Item item, int codeNumber) throws Exception {
+
+    }
+}
---
+++ Vending machine/IdleState.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class IdleState implements State{
+    public IdleState(){
+        System.out.println("currenlty the vending machine is in idle state");
+    }
+    public IdleState(VendingMachine vendingMachine){
+        System.out.println("Currently vending machine is in idle state");
+
+    }
+
+    @Override
+    public void clickOnInsertCoinButton(VendingMachine vendingMachine) throws Exception {
+
+    }
+
+    @Override
+    public void clickOnStartProductSelectionButton(VendingMachine vendingMachine) throws Exception {
+        throw new Exception("first you need to click on insert coin button");
+    }
+
+    @Override
+    public void insertCoin(VendingMachine vendingMachine, Coin coin) throws Exception {
+        throw new Exception("you cannot insert coin in idle state");
+    }
+
+    @Override
+    public void chooseProduct(VendingMachine machine, int codeNumber) throws Exception {
+        throw new Exception("you cannot choose product in idle state");
+    }
+
+    @Override
+    public int getChange(int returnChangeMoney) throws Exception {
+        throw new Exception("you cannot get change in idle state");
+    }
+
+    @Override
+    public Item dispenseProduct(VendingMachine vendingMachine, int codeNumber) throws Exception {
+        throw new Exception("Product cannot be dispenced in idle state");
+    }
+
+    @Override
+    public List<Coin> refundFullMoney(VendingMachine vendingMachine) throws Exception {
+        throw new Exception("you cannot get refund in idle state");
+    }
+
+    @Override
+    public void updateInventory(VendingMachine vendingMachine, Item item, int codeNumber) throws Exception {
+
+    }
+}
